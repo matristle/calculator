afe1ac104b7550a39b2c25a331a946bea277a417
mercerary <mercerary@tutanota.com>
2022-02-27 17:36:05 +0200

    Make standard functional calculator prototype

--- script.js
+++ script.js
@@ -1,44 +1,212 @@
-let firstNumber, operatorSign, secondNumber;
 
-const display = document.querySelector('#result-display');
+const display      = document.querySelector('#result-display');
+const calcuButtons = document.querySelectorAll('.non-clear-buttons button');
+const clearButton  = document.getElementById('clear-button');
+const acButton     = document.getElementById('ac-button');
 
-const numberButtons = document.getElementById('number-buttons').childNodes;
+display.textContent = '0';
 
-for (let i = 0; i < numberButtons.length; i++)
+let expr = 
 {
-    numberButtons[i].addEventListener('click', () => 
+    leftNumber   : '',
+    rightNumber  : '',
+    operatorSign : '',
+    total        : '',
+    start        : true,
+    operate()
+    {   
+        switch(this.operatorSign)
+        {
+            case '+':
+                return add(this.leftNumber, this.rightNumber);
+                break;
+            
+            case '-':
+                return subtract(this.leftNumber, this.rightNumber);
+                break;
+                
+            case 'x' || '*':
+                return multiply(this.leftNumber, this.rightNumber);
+                break;
+            
+            case '/':
+                return divide(this.leftNumber, this.rightNumber);
+                break;
+
+
+
+            default:
+                return `ERROR: INVALID OPERATOR`;
+        }   
+    }
+};
+
+calcuButtons.forEach( (button) =>
+{
+    button.addEventListener('click', () => 
     {
-        display.textContent = numberButtons[i].textContent;
-        secondNumber = display.textContent; 
+        if (   button.textContent === '=' 
+            || button.textContent === '+' 
+            || button.textContent === '-' 
+            || button.textContent === 'x' 
+            || button.textContent === '/')
+        {
+            if (expr.leftNumber === '0' && expr.operatorSign === '/' && expr.rightNumber === '0')
+            {
+                display.textContent = `Please don't do that one...`;
+                return;
+            }
+            else if (expr.leftNumber !== '' && expr.rightNumber !== '')
+            {
+                    expr.leftNumber = expr.operate();
+    
+                    display.textContent = expr.leftNumber;
+                    expr.total = expr.leftNumber;
+                    expr.rightNumber = '';
+                    expr.operatorSign = '';
+               
+                if (button.textContent === '=')
+                {
+                    display.textContent = expr.total;
+                    expr.start = true;
+                    return;
+                }
+            }
+
+            if (expr.operatorSign !== '')
+            {
+                if (button.textContent === '=')
+                {
+                    display.textContent = expr.leftNumber;
+                    expr.operatorSign = '';
+                }
+                else 
+                {
+                    display.textContent = display.textContent.replace(/[\+\-\x\/]/, button.textContent);
+                }
+
+            }
+            else
+            {
+                display.textContent += ` ${button.textContent}`;
+            }
+
+            if (button.textContent !== '=')
+            {
+                expr.operatorSign = button.textContent;
+            }
+
+        }
+        else if (button.textContent !== '0' && display.textContent === '0')
+        {
+            if (countDots(display.textContent) === 0)
+            {
+                display.textContent = button.textContent;
+                expr.leftNumber = button.textContent;
+            }
+        }
+        else if (button.textContent === '⌫')
+        {
+            display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+
+
+            if (expr.operatorSign === '' && expr.rightNumber === '')
+            {
+                expr.leftNumber = expr.leftNumber.slice(0, expr.leftNumber.length - 1);
+            }
+            else if (expr.operatorSign !== '' && expr.rightNumber === '')
+            {
+                expr.operatorSign = '';
+            }
+            else if (expr.operatorSign !== '' && expr.rightNumber !== '')
+            {
+                expr.rightNumber = expr.rightNumber.slice(0, expr.rightNumber.length - 1);
+            }           
+        }
+        else if (expr.start)
+        {
+            display.textContent = button.textContent;
+            expr.leftNumber = button.textContent;
+            expr.start = false;
+            return;
+        }
+        else if (button.textContent === '.')         
+        {
+           if (countDots(expr.leftNumber) === 1 && expr.rightNumber === '')
+           {
+                return;
+           } 
+           else if (expr.leftNumber !== '' && countDots(expr.leftNumber) === 0)
+           {
+               display.textContent += button.textContent;
+               expr.leftNumber += button.textContent;
+           }
+           else if ( countDots(display.textContent) === 2 || potentialConsecutiveDot(display.textContent) )
+           {
+                
+                return;
+           }
+           else if ( countDots(display.textContent) === 1)
+           {
+                display.textContent += button.textContent;
+                expr.rightNumber += button.textContent;
+           }
+   
+        }
+        else if (expr.operatorSign === ''  &&  expr.rightNumber === '')
+        {
+            if (display.textContent === '-' || display.textContent === '+')
+            {
+                display.textContent += button.textContent;
+                expr.leftNumber = display.textContent;
+            }
+            else 
+            {
+                display.textContent += button.textContent;
+                expr.leftNumber += button.textContent;
+            }
+
+           
+            if (expr.leftNumber === '')                 
+            {
+                expr.leftNumber = button.textContent;
+            }
+            
+        } 
+        else if (expr.leftNumber !== ''  &&  expr.operatorSign !== ''  &&  expr.rightNumber === '')
+        {
+            display.textContent += ` ${button.textContent}`;
+            
+            expr.rightNumber = button.textContent; 
+        } 
+        else if (expr.leftNumber !== ''  &&  expr.operatorSign !== ''  &&  expr.rightNumber !== '')
+        {
+            display.textContent += button.textContent;
+            expr.rightNumber += button.textContent;
+        }
+        
     });
-}
-
-const operatorButtons = document.getElementById('operator-buttons').childNodes;
-
-for (let i = 0; i < operatorButtons.length; i++)
-{
-    operatorButtons[i].addEventListener('click', () => 
-    {
-        firstNumber = display.textContent; // stores the first single-digit number entered and displayed, not the operator
-        display.textContent = operatorButtons[i].textContent;
-        operatorSign = display.textContent;
-    });
-}
-
-const equalButton = document.getElementById('equal-button');
-
-equalButton.addEventListener('click', () => 
-{
-    display.textContent = operate(firstNumber, operatorSign, secondNumber);
 });
 
-const clearButton = document.getElementById('clear-button');
-
-clearButton.addEventListener('click', () => 
+clearButton.addEventListener('click', () =>
 {
-    display.textContent = '';
+    display.textContent = '0';
+    expr.start = true;
+
 });
 
+acButton.addEventListener('click', () =>
+{
+    display.textContent = '0';
+
+    for (let x in expr)
+    {
+        if( expr[x] === expr['operate'] ) continue;
+        else expr[x] = '';
+    }
+
+    expr.start = true;
+});
 
 
 
@@ -62,28 +230,29 @@ function divide(a, b)
     return a / b;
 }
 
-
-function operate(a, operator, b)
+function potentialConsecutiveDot(string)
 {
-    switch(operator)
+    let splitString = string.split('');
+
+    let dotPushTestString = splitString.concat('.');
+    
+    for (let i = 0; i < dotPushTestString.length; i++)
     {
-        case '+':
-            return add(a, b);
-            break;
-         
-        case '-':
-            return subtract(a, b);
-            break;
-            
-        case 'x' || '*':
-            return multiply(a, b);
-            break;
-        
-        case '/':
-            return divide(a, b);
-            break;
-        default:
-            return `ERROR`;
-    }   
+        if (dotPushTestString[i] === dotPushTestString[i-1]) return true;
+    }
+
+    return false;
 }
 
+function countDots(string)
+{
+    let dotCount = 0;
+    
+    string.split('').forEach( (substring) =>
+    {
+
+        if (substring === '.') dotCount++;
+    });
+
+    return dotCount;
+}
